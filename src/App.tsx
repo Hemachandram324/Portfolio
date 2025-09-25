3dfb1df77049a3a44ce80b970114c0ca2aabc79d
Hemachandram <hemachandram324@gmail.com>
2025-09-25 17:52:53 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
-import HomePage from "@/react-app/pages/Home";
-import AboutPage from "@/react-app/pages/About";
-import ExperiencePage from "@/react-app/pages/Experience";
-import ProjectsPage from "@/react-app/pages/Projects";
-import ContactPage from "@/react-app/pages/Contact";
-import Navigation from "@/react-app/components/Navigation";
-import MatrixRain from "@/react-app/components/MatrixRain";
-import ParticleField from "@/react-app/components/ParticleField";
+import HomePage from "./react-app/pages/Home";
+import AboutPage from "./react-app/pages/About";
+import ExperiencePage from "./react-app/pages/Experience";
+import ProjectsPage from "./react-app/pages/Projects";
+import ContactPage from "./react-app/pages/Contact";
+import Navigation from "./react-app/components/Navigation";
+import MatrixRain from "./react-app/components/MatrixRain";
+import ParticleField from "./react-app/components/ParticleField";
 
 export default function App() {
   return (
